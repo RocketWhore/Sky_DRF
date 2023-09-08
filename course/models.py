0ca8cdf170891b7cc91e3216804c150efd3f7a95
GP <gladkov@edu.yaconnect.com>
2023-09-08 22:29:23 +0300

    add payment

--- course/models.py
+++ course/models.py
@@ -32,3 +32,22 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+class Payment(models.Model):
+    CHOICES = (
+        ("Card", "карта"),
+        ("CASH", "наличка"),
+    )
+    user = models.ForeignKey(User, verbose_name='владелец', on_delete=models.CASCADE, **NULLABLE)
+    date_of_pay = models.DateTimeField(auto_now_add=True, **NULLABLE)
+    item = models.ForeignKey(Course, on_delete=models.SET_NULL, **NULLABLE)
+    lesson_name = models.ForeignKey(Lesson, on_delete=models.SET_NULL, **NULLABLE)
+    cash = models.PositiveIntegerField()
+    payment_method = models.CharField(choices=CHOICES, verbose_name="тип оплаты")
+
+    def str(self):
+        return f"{self.user} {self.item}"
+
+    class Meta:
+        verbose_name = "оплата"
+        verbose_name_plural = "оплаты"
